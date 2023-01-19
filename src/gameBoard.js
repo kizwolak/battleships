65428ff26f4565b68a4d0e168e59cd01d99955fc
kizwolak <ki.zwolak@gmail.com>
2023-01-19 02:52:14 +0100

    Fix factory logic/board generator

--- src/gameBoard.js
+++ src/gameBoard.js
@@ -1,13 +1,17 @@
 export default function gameBoard() {
+    function board() {
+        const boardToReturn = new Array(10).fill(new Array(10).fill(0));
+        return boardToReturn;
+    }
     return {
-        board: function() {
-            const boardToReturn = new Array(10).fill(new Array(10).fill(0));
-            return boardToReturn;
+        board: board(),
+        place: function(coordinates) {
+            return coordinates;
         },
         missedAttacks: [],        
     }
 };
 
 const test = gameBoard();
-const testBoard = test.board();
-console.log(testBoard);
+console.log(test.board);
+console.log(test.place([[1,2], [2,1]]))
--- src/gameBoard.test.js
+++ src/gameBoard.test.js
@@ -1,7 +1,12 @@
 import gameBoard from "./gameBoard";
+import Ship from "./ship";
 const gameBoardTest = gameBoard();
-const testBoard = gameBoardTest.board();
 
-it('gameBoard generated properly', () => {
-    expect(testBoard[1][2]).toBe(0);
+it.only('gameBoard generated properly', () => {
+    expect(gameBoardTest.board[1][2]).toBe(0);
 });
+
+it('place ships properly while making sure no other place is taken', () => {
+    gameBoard.place(Ship(3), [testBoard[1][1], testBoard[1][2], testBoard[1][3]]);
+    expect(testBoard[1][2]).toBe(true);
+})
