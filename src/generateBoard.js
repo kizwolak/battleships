d11d35e1f542603f715e5c54ebbcaa1ddcbc5076
kizwolak <ki.zwolak@gmail.com>
2023-01-20 05:20:57 +0100

    Hide menu when board activates

--- src/generateBoard.js
+++ src/generateBoard.js
@@ -1,1 +1,20 @@
 import gameBoard from './gameBoard.js';
+
+export default function generateBoard() {
+  const title = document.querySelector('.title');
+  const newGameButton = document.querySelector('.newGameButton');
+  const webBoard = document.querySelector('.board');
+  title.classList.add('deactivated');
+  newGameButton.classList.add('deactivated');
+  webBoard.classList.add('activated');
+  const board = gameBoard();
+  console.log(board.board);
+  board.board.forEach((cell) => {
+    cell.forEach((subCell) => {
+      const boardCell = document.createElement('div');
+      boardCell.textContent = 'Cell';
+      boardCell.classList = 'cell';
+      webBoard.appendChild(boardCell);
+    });
+  });
+}
--- src/main.js
+++ src/main.js
@@ -1,17 +1,7 @@
 import './style.css';
 import gameBoard from './gameBoard.js';
 import Ship from './ship.js';
-
-const board = gameBoard();
-console.log(board.board);
-const webBoard = document.querySelector('.board');
-board.board.forEach((cell) => {
-  cell.forEach((subCell) => {
-    const boardCell = document.createElement('div');
-    boardCell.textContent = 'Cell';
-    webBoard.appendChild(boardCell);
-  });
-});
+import generateBoard from './generateBoard.js';
 
 const newGameButton = document.querySelector('.newGameButton');
-newGameButton.addEventListener('click');
+newGameButton.addEventListener('click', generateBoard);
