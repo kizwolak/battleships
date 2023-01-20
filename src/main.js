910d22fa36d8f3b0b0246247829db38c3915b43e
kizwolak <ki.zwolak@gmail.com>
2023-01-20 04:34:07 +0100

    Generate board

--- src/main.js
+++ src/main.js
@@ -2,5 +2,16 @@ import './style.css';
 import gameBoard from './gameBoard.js';
 import Ship from './ship.js';
 
-const test = gameBoard();
-test.place([1, 2], [2, 3], [3, 4]);
+const board = gameBoard();
+console.log(board.board);
+const webBoard = document.querySelector('.board');
+board.board.forEach((cell) => {
+  cell.forEach((subCell) => {
+    const boardCell = document.createElement('div');
+    boardCell.textContent = 'Cell';
+    webBoard.appendChild(boardCell);
+  });
+});
+
+const newGameButton = document.querySelector('.newGameButton');
+newGameButton.addEventListener('click');
