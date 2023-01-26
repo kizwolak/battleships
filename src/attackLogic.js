14218a59a679a8b8ef3762e319242c2151dab98d
kizwolak <ki.zwolak@gmail.com>
2023-01-26 05:28:16 +0100

    Attacks allowed correctly

--- src/attackLogic.js
+++ src/attackLogic.js
@@ -1,13 +1,17 @@
 import numberGenerator from './numberGenerator.js';
 
 export default function attackLogic(board1, board2) {
+  function isArrayInArray(arr, item) {
+    const itemAsString = JSON.stringify(item);
+    const contains = arr.some((ele) => JSON.stringify(ele) === itemAsString);
+    return contains;
+  }
   const cpuAttacks = [];
   const board1Cells = document.querySelectorAll('.cell1');
   const board2Cells = document.querySelectorAll('.cell2');
   board2Cells.forEach((cell) => {
     function boardCellClick() {
       const cell2Contents = `[${cell.textContent}]`;
-      console.log(cell2Contents);
       if (board2.receiveAttack(JSON.parse(cell2Contents)) === true) {
         cell.classList = '';
         cell.classList.remove('empty');
@@ -23,22 +27,23 @@ export default function attackLogic(board1, board2) {
         if (sunkenCounter2 === 5) alert('Human wins!');
       });
       const randomCPUattack = numberGenerator(0, 9, cpuAttacks);
-      const cell1Contents = `[${cell.textContent}]`;
-      console.log(cell1Contents);
-      if (board2.receiveAttack(JSON.parse(cell1Contents)) === true) {
-        cell1.classList = '';
-        cell1.classList.remove('empty');
-        cell1.classList.remove('cell2');
-        cell1.classList.add('cellTakenByPlayer');
-      } else if (board2.receiveAttack(JSON.parse(cell1Contents)) === false) {
-        cell1.classList = 'empty';
-        cell1.removeEventListener('click', boardCellClick);
-      }
-      const sunkenCounter1 = 0;
-      board1.ships.forEach((ship) => {
-        if (ship.isSunkenProperty === true) sunkenCounter1 += 1;
-        if (sunkenCounter1 === 5) alert('Robot wins!');
-      });
+      console.log(randomCPUattack);
+      console.log(board1.board);
+    //   if (isArrayInArray(board1.board[numberGenerator[0]][numberGenerator[1]])) console.log('exists in board!');
+    //   if (board2.receiveAttack(JSON.parse(cell1Contents)) === true) {
+    //     cell1.classList = '';
+    //     cell1.classList.remove('empty');
+    //     cell1.classList.remove('cell2');
+    //     cell1.classList.add('cellTakenByPlayer');
+    //   } else if (board2.receiveAttack(JSON.parse(cell1Contents)) === false) {
+    //     cell1.classList = 'empty';
+    //     cell1.removeEventListener('click', boardCellClick);
+    //   }
+    //   const sunkenCounter1 = 0;
+    //   board1.ships.forEach((ship) => {
+    //     if (ship.isSunkenProperty === true) sunkenCounter1 += 1;
+    //     if (sunkenCounter1 === 5) alert('Robot wins!');
+    //   });
     }
     cell.addEventListener('click', boardCellClick);
   });
--- src/generateBoard.js
+++ src/generateBoard.js
@@ -3,6 +3,8 @@ import placeShips from './placeShips.js';
 import placeShipsAI from './placeShipsAI.js';
 
 export default function generateBoard() {
+  const cpuAttacks = [];
+  const isReady = false;
   const title = document.querySelector('.title');
   const newGameButton = document.querySelector('.newGameButton');
   const board1Container = document.querySelector('.board1Container');
@@ -33,10 +35,32 @@ export default function generateBoard() {
   });
   board2.board.forEach((cell) => {
     const boardCell = document.createElement('div');
+    function boardCellClick() {
+      const cellContents = `[${boardCell.textContent}]`;
+      console.log(cellContents);
+      if (board2.receiveAttack(JSON.parse(cellContents)) === true) {
+        boardCell.classList = '';
+        boardCell.classList.remove('empty');
+        boardCell.classList.remove('cell2');
+        boardCell.classList.add('cellTakenByPlayer');
+      } else if (board2.receiveAttack(JSON.parse(cellContents)) === false) {
+        boardCell.classList = 'empty';
+        boardCell.removeEventListener('click', boardCellClick);
+      }
+
+      let sunkenCounter = 0;
+      board2.ships.forEach((ship) => {
+        if (ship.isSunkenProperty === true) sunkenCounter += 1;
+        if (sunkenCounter === 5) alert('Human wins!');
+      });
+      board1.ships.forEach((ship) => {
+        if (ship.isSunkenProperty === true) sunkenCounter += 1;
+        if (sunkenCounter === 5) alert('Robot wins!');
+      });
+    }
     boardCell.textContent = `${cell[0]}, ${cell[1]}`;
     boardCell.classList = 'cell2';
     webBoard2.appendChild(boardCell);
-    boardCell.addEventListener('click', boardCellClick);
   });
   placeShips(board1, board2);
   placeShipsAI(board2);
